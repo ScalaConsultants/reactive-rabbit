d3f960f5f4f57e05c0feadc0dbf3a0c790a003e1
Michał Kiędyś <michal@kiedys.net>
2014-10-08 21:14:43 +0200

    Add case class for routing keys

---
+++ src/main/scala/io/scalac/amqp/RoutingKey.scala
@@ -0,0 +1,7 @@
+package io.scalac.amqp
+
+
+/** The routing key is used for routing messages depending on the exchange configuration. */
+case class RoutingKey(key: String) {
+  require(key.length <= 255, "key.length > 255")
+}
