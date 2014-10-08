5f4a3ca62d1a355736884fbbeb8f3247c75e7319
Michał Kiędyś <michal@kiedys.net>
2014-10-08 17:44:17 +0200

    Remove Exchange.Default

--- src/main/scala/io/scalac/amqp/Exchange.scala
+++ src/main/scala/io/scalac/amqp/Exchange.scala
@@ -1,18 +1,6 @@
 package io.scalac.amqp
 
 
-object Exchange {
-  /** The default exchange is a direct exchange with no name (empty string) pre-declared by the broker.
-    * It has one special property that makes it very useful for simple applications: every queue that is created
-    * is automatically bound to it with a routing key which is the same as the queue name. */
-  val Default = apply(
-    name = "",
-    `type` = Direct,
-    durable = true,
-    autoDelete = false,
-    arguments = Map())
-}
-
 case class Exchange(name: String,
                     `type`: Type,
                     durable: Boolean,
