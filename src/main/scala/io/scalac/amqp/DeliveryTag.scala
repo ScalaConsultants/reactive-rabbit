2d71a48b4b26767c991e3fe3827d1a6e3c9bb83a
Michał Kiędyś <michal@kiedys.net>
2014-10-09 10:36:33 +0200

    Replace Publisher with Subscription

--- src/main/scala/io/scalac/amqp/DeliveryTag.scala
+++ src/main/scala/io/scalac/amqp/DeliveryTag.scala
@@ -1,7 +1,7 @@
 package io.scalac.amqp
 
 /** The server-assigned and channel-specific delivery tag assigned to each individual [[io.scalac.amqp.Delivery]].
-  * The delivery tag is valid only within the [[org.reactivestreams.Publisher]] from which the message was received. */
+  * The delivery tag is valid only within the [[org.reactivestreams.Subscription]] from which the message was received. */
 final case class DeliveryTag(underlying: Long) extends AnyVal {
   def toLong = underlying
 }
