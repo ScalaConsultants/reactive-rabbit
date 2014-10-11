25fb519faed8daf223aec4fa0dda367a0513eb6c
Michał Kiędyś <michal@kiedys.net>
2014-10-11 18:49:07 +0200

    Add some TCK tests for QueuePublisher

--- build.sbt
+++ build.sbt
@@ -16,8 +16,13 @@ scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Xfatal-warning
 libraryDependencies ++= Seq(
   "org.reactivestreams" % "reactive-streams" % "0.4.0",
   "com.rabbitmq" % "amqp-client" % "3.3.5",
-  "joda-time" % "joda-time" % "2.5",            // for DateTime
+  "joda-time" % "joda-time" % "2.5",              // for DateTime
   "org.joda" % "joda-convert" % "1.7",
-  "com.typesafe.akka" %% "akka-actor" % "2.3.6",// for ByteString
-  "com.google.guava" % "guava" % "18.0"         // for MediaType
+  "com.typesafe.akka" %% "akka-actor" % "2.3.6",  // for ByteString
+  "com.google.guava" % "guava" % "18.0",          // for MediaType
+  "com.google.code.findbugs" % "jsr305" % "3.0.0",
+  "org.scalatest" %% "scalatest" % "2.2.1" % "test",
+  "com.google.inject" % "guice" % "3.0" % "test", // to make sbt happy
+  "org.reactivestreams" % "reactive-streams-tck" % "0.4.0" % "test",
+  "com.typesafe" % "config" % "1.2.1" % "test"    // Broker config
 )
---
+++ src/test/scala/io/scalac/amqp/impl/QueuePublisherSpec.scala
@@ -0,0 +1,49 @@
+package io.scalac.amqp.impl
+
+import java.util.UUID
+
+import com.google.common.collect.ImmutableMap
+import com.rabbitmq.client.{AMQP, ConnectionFactory}
+
+import io.scalac.amqp.Delivery
+
+import org.reactivestreams.tck.{PublisherVerification, TestEnvironment}
+import org.reactivestreams.{Subscriber, Publisher}
+import org.scalatest.testng.TestNGSuiteLike
+
+
+/** You need to have RabbitMQ server to run these tests.
+  * Here you can get it: https://www.rabbitmq.com/download.html */
+class QueuePublisherSpec(env: TestEnvironment, publisherShutdownTimeout: Long)
+  extends PublisherVerification[Delivery](env, publisherShutdownTimeout) with TestNGSuiteLike {
+
+  def this() = this(new TestEnvironment(600L), 1000L)
+
+  val props = new AMQP.BasicProperties.Builder().build()
+  val factory = new ConnectionFactory
+  val connection = factory.newConnection()
+  val channel = connection.createChannel()
+
+  override def createPublisher(elements: Long): Publisher[Delivery] = {
+    val queue = channel.queueDeclare(UUID.randomUUID().toString,
+      false, false, false, ImmutableMap.of("x-expires", 30000.asInstanceOf[Object])).getQueue
+    1L.to(elements).foreach(_ => channel.basicPublish("", queue, props, Array[Byte]()))
+    new QueuePublisher(connection, queue)
+  }
+
+  // TODO(mkiedys): impl
+  override def createErrorStatePublisher(): Publisher[Delivery] = new Publisher[Delivery] {
+    override def subscribe(s: Subscriber[_ >: Delivery]): Unit = {
+      s.onError(new IllegalStateException("Unable to serve subscribers right now!"))
+    }
+  }
+
+  override def createPublisher1MustProduceAStreamOfExactly1Element() = ()
+  override def createPublisher3MustProduceAStreamOfExactly3Elements() = ()
+  override def spec102_maySignalLessThanRequestedAndTerminateSubscription() = ()
+  override def spec103_mustSignalOnMethodsSequentially() = ()
+  override def spec105_mustSignalOnCompleteWhenFiniteStreamTerminates() = ()
+  override def spec107_mustNotEmitFurtherSignalsOnceOnCompleteHasBeenSignalled() = ()
+  override def spec317_mustSupportACumulativePendingElementCountUpToLongMaxValue() = ()
+  override def spec317_mustSupportAPendingElementCountUpToLongMaxValue() = ()
+}
