77a8d7c9e8a2653ae7c2ecf266d552ce05c3e456
Michał Kiędyś <michal@kiedys.net>
2014-10-15 13:48:25 +0200

    Remove redundant parenthesis

--- src/main/scala/io/scalac/amqp/impl/ExchangeSubscriber.scala
+++ src/main/scala/io/scalac/amqp/impl/ExchangeSubscriber.scala
@@ -18,14 +18,13 @@ private[amqp] class ExchangeSubscriber(channel: Channel, exchange: String, routi
     subscription.request(1)
   }
 
-  override def onNext(message: Message) = {
+  override def onNext(message: Message) =
     channel.basicPublish(
       exchange,
       routingKey,
       Conversions.toBasicProperties(message),
       message.body.toArray)
     subscription.get().request(1)
-  }
 
   override def onError(t: Throwable) =
     channel.close()
