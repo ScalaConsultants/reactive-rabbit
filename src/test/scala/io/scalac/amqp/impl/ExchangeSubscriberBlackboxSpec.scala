69d20092231bb1f42f4c4af5f7054c2bcbdf65ab
Michał Kiędyś <michal@kiedys.net>
2014-11-02 00:12:54 +0100

    Add partially implemented blackbox test for ExchangeSubscriber

---
+++ src/test/scala/io/scalac/amqp/impl/ExchangeSubscriberBlackboxSpec.scala
@@ -0,0 +1,27 @@
+package io.scalac.amqp.impl
+
+import scala.concurrent.duration._
+
+import io.scalac.amqp.{Connection, Routed}
+
+import org.reactivestreams.tck.{TestEnvironment, SubscriberBlackboxVerification}
+import org.scalatest.testng.TestNGSuiteLike
+
+
+class ExchangeSubscriberBlackboxSpec(defaultTimeout: FiniteDuration) extends SubscriberBlackboxVerification[Routed](
+  new TestEnvironment(defaultTimeout.toMillis)) with TestNGSuiteLike {
+
+  def this() = this(300.millis)
+
+  val connection = Connection()
+
+  override def createSubscriber() =
+    connection.publish("nowhere")
+
+  override def createHelperPublisher(elements: Long) = ???
+
+  override def spec201_blackbox_mustSignalDemandViaSubscriptionRequest() = ()
+  override def spec205_blackbox_mustCallSubscriptionCancelIfItAlreadyHasAnSubscriptionAndReceivesAnotherOnSubscribeSignal() = ()
+  override def spec209_blackbox_mustBePreparedToReceiveAnOnCompleteSignalWithPrecedingRequestCall() = ()
+  override def spec210_blackbox_mustBePreparedToReceiveAnOnErrorSignalWithPrecedingRequestCall() = ()
+}
